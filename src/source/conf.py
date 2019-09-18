8903afbd77f9a0631721cc99ebc060fe6ca8e21d
José Miguel Sánchez Alés <sio2sio2+github@gmail.com>
2019-09-18 21:54:20 +0200

    Modifica la generación del HTML

--- src/source/conf.py
+++ src/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
