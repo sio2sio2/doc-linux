21bb99c0c0290274c851155cba8763c081db95be
José Miguel Sánchez Alés <sio2sio2+github@gmail.com>
2021-06-06 21:48:58 +0200

    Añade mejoras para el tratamineto de argumentos en los scripts

--- src/source/03.scripts/06.misc/files/args_artesanal.sh
+++ src/source/03.scripts/06.misc/files/args_artesanal.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-# Analiza una opción que requiera argumento.
-# $1: La opción
-# $2: El argumento de la opción.
-# $3: 1, si el argumento es opcional.
-# Devuelve:
-# 0: Si se pasó el argumento.
-# 1: Si no se pasó argumento
-# Imprime:
-# El argumento suministrado.
-optarg() {
-   if [ -z "$2" ] || echo "$2" | grep -q '^-.'; then
-      [ "$3" != "1" -a "$OPTERR" != "0" ] && echo "$1 requiere argumento" >&2
-      exit 1
-   else
-      echo "$2"
-   fi
-}
-
 help() {
    echo "$(basename $0): Ilustra el procesamiento de las opciones de un script.
 
@@ -33,40 +15,62 @@ Opciones:
 # Si se sale con un error, se muestra la ayuda.
 trap '[ $? -ne 0 ] && help' EXIT
 
-# Análisis de las opciones.
-while [ $# -gt 0 ]; do
-   case $1 in
-      -h|--help)  # Ayuda
-         help
-         exit 0
-         ;;
-      -f|--file)  # Opción con argumento obligatorio.
-         FICHERO=$(optarg "$1" "$2")
-         [ $? -eq 0 ] || exit 2
-         shift 2
-         ;;
-      -p|--password)  # Opción con argumento opcional.
-         PASSWORD=$(optarg "$1" "$2" 1)
-         [ $? -eq 0 ] && shift
-         shift
-         ;;
-      -v|--verbose)  # Opción sin argumento
-         VERBOSE=1
-         shift
-         ;;
-      --)  # Final de las opciones
-         shift
-         break
-         ;;
-      -?*)
-         echo "$1: Opción desconocida" >&2
-         exit 2
-         ;;
-      *) # Argumento posicional no asociado a opción: acabamos
-         break
-         ;;
-   esac
-done
+requiere_parametro() {
+   expr "$1" : ".*$2:" > /dev/null
+}
 
-# $@ contiene el resto de argumentos sin procesar.
-echo "$@"
+
+opts="hp:f:v"  # ":" significa que la opción requiere argumento propio
+
+{ ### Análisis de las opciones.
+   while [ $# -gt 0 ]; do
+      case "$1" in
+         -h|--help)  # Ayuda
+            help
+            exit 0
+            ;;
+         -f|--file)  # Opción con argumento obligatorio.
+            [ -z "${2%%-*}" ] && echo "$1 requiere argumento" >&2 && exit 2
+            ARCHIVO="$2"
+            shift
+            ;;
+         -p|--password)  # Opción con argumento opcional.
+            [ -z "${2%%-*}" ] && echo "$1 requiere argumento" >&2 && exit 2
+            PASSWORD="$2"
+            shift
+            ;;
+         -v|--verbose)  # Opción sin argumento
+            VERBOSE=1
+            ;;
+         --)  # Final de las opciones
+            shift
+            break
+            ;;
+         --*|-?)
+            echo "$1: Opción desconocida"
+            exit 2
+            ;;
+         -??*)  # Separa opciones cortas y lo vuelve a intentar
+            arg=$(printf "%.2s" "$1")
+            arg=${arg#?}
+            if requiere_parametro "$opts" "$arg"; then
+               rarg="${1#-$arg}" 
+            else
+               rarg="-${1#-$arg}" 
+            fi
+            shift
+            set -- -"$arg" "$rarg" "$@"
+            continue
+            ;;
+         *)
+            break
+      esac
+      shift
+   done
+}
+
+
+echo "VERBOSE=$VERBOSE
+ARCHIVO=$ARCHIVO
+CONTRASEÑA=$PASSWORD
+Posicionales: $*"
